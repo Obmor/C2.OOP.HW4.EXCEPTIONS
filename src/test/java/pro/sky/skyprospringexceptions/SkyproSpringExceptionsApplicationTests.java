ffc9d77159041effab8f9cea89b805586704eaa5
Obmor <terekhin.alex@mail.ru>
2024-11-15 14:00:14 +0300

    TheFirstLook

---
+++ src/test/java/pro/sky/skyprospringexceptions/SkyproSpringExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
